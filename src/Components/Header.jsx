6814d9b205f2c1b921f3791e2a38b142f7573602
snehalkdm2191 <snehalkdm2191@gmail.com>
2021-09-08 02:44:16 +0530

    React router added for navigation.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import NavBar from '../Components/NavBar';
 import HomeBg from '../Assets/img/HomePage-bg.mp4';
 
 function Header() {
   return (
     <div>
-      <NavBar/>
       <video width="100%" autoPlay playsInline loop muted>
       <source src={HomeBg} type="video/mp4"/>
       </video>
